de310775bb00ab3cfd60488b3bbe641fc5f7068f
Randall Degges <r@rdegges.com>
2014-04-16 14:28:55 -0700

    Adding routes for /, /register, /login, /dashboard.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/register', function(req, res) {
+  res.render('register', { title: 'Express' });
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Express' });
+});
+
+router.get('/dashboard', function(req, res) {
+  res.render('dashboard', { title: 'Express' });
+});
+
+
 module.exports = router;
