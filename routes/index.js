bc9b8d82ebb4fc38fda4a9838c1f1bf80082ded2
Randall Degges <r@rdegges.com>
2014-04-21 10:41:34 -0700

    Adding in some preliminary route code.

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,71 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
+
+// Render the home page.
 router.get('/', function(req, res) {
   res.render('index', { title: 'Home' });
 });
 
+
+// Render the registration page.
 router.get('/register', function(req, res) {
-  res.render('register', { title: 'Express' });
+  res.render('register', { title: 'Register' });
 });
 
+
+// Render the login page.
 router.get('/login', function(req, res) {
-  res.render('login', { title: 'Express' });
+  res.render('login', { title: 'Login' });
 });
 
-router.get('/dashboard', function(req, res) {
-  res.render('dashboard', { title: 'Express' });
+
+// Logout the user, then redirect to the home page.
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
+
+//router.post('/login', passport.authenticate('stormpath', {successRedirect: '/dashboard', failureRedirect: '/login'}));
+router.post('/login', function(req, res, next) {
+  passport.authenticate('stormpath', function(err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (!user && info) {
+      return res.render('login', {
+        error: info,
+        username: req.body.username,
+      });
+    }
+    req.logIn(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect('/dashboard');
+    });
+  })(req, res, next);
+});
+
+
+// Render the dashboard page.
+router.get('/dashboard', function (req, res) {
+  if (!req.user) {
+    return res.redirect('/login');
+  }
+
+  res.render('dashboard', {
+    title: 'Express',
+    user: req.user, // {
+      //email: 'r@rdegges.com',
+      //custom_data: {
+      //  birthday: '06/28/1988',
+      //  color: 'black',
+      //}
+    }
+  );
+});
 
 module.exports = router;
