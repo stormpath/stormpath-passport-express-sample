ce0c60f2eca20a028111d42c2108178f425f385b
Randall Degges <r@rdegges.com>
2014-04-21 10:32:34 -0700

    Updating the HTML title for our home page.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
 
 router.get('/register', function(req, res) {
