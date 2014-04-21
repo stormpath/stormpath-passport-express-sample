543397638653282c950a246cadade58c6997e79b
Randall Degges <r@rdegges.com>
2014-04-21 14:49:19 -0700

    Adding a registration view.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var stormpath = require('stormpath');
 
 
 // Render the home page.
@@ -12,6 +13,49 @@ router.get('/', function(req, res) {
 // Render the registration page.
 router.get('/register', function(req, res) {
   res.render('register', { title: 'Register', error: req.flash('error')[0] });
+});
+
+
+// Register a new user to Stormpath.
+router.post('/register', function(req, res) {
+
+  var username = req.body.username;
+  var password = req.body.password;
+
+  // Grab user fields.
+  if (!username || !password) {
+    return res.render('register', { title: 'Register', error: 'Email and password required.' });
+  }
+
+  // Initialize our Stormpath client.
+  var apiKey = new stormpath.ApiKey(
+    process.env['STORMPATH_API_KEY_ID'],
+    process.env['STORMPATH_API_KEY_SECRET']
+  );
+  var spClient = new stormpath.Client({ apiKey: apiKey });
+
+  // Grab our app, then attempt to create this user's account.
+  var app = spClient.getApplication(process.env['STORMPATH_APP_HREF'], function(err, app) {
+    if (err) throw err;
+
+    app.createAccount({
+      givenName: 'John',
+      surname: 'Smith',
+      username: username,
+      email: username,
+      password: password,
+    }, function (err, createdAccount) {
+      if (err) {
+        return res.render('register', {'title': 'Register', error: err.userMessage });
+      } else {
+        passport.authenticate('stormpath')(req, res, function () {
+          return res.redirect('/dashboard');
+        });
+      }
+    });
+
+  });
+
 });
 
 
